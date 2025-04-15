d15f0576cb6b70923367f662379ce2852903f4f8
Munnazzar <munnzzar0214@gmail.com>
2025-04-15 22:55:35 +0500

    forntnedurl coords

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://attendancefrontend25.vercel.app/", // Frontend URL
+    origin: "https://attendancefrontend25.vercel.app", // Frontend URL
     credentials: true, // Allow credentials (cookies, authorization headers)
   })
 );
