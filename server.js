46575fb802059bae5d01ac2c3490cbae438eb22b
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-11-09 11:16:52 +0500

    Added frontend URL

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://coderscup25-attendance-frontend.vercel.app/"], // Frontend URLs
+    origin: ["http://localhost:5173","https://coderscup25-attendance-frontend.vercel.app"], // Frontend URLs
     credentials: true, // Allow credentials (cookies, authorization headers)
   })
 );
