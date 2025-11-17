e5fa6cd2ee5c725654ecf7481a373ce9b27120a8
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-11-17 19:30:40 +0500

    updated event check

---
+++ utils/password-gen.js
@@ -0,0 +1,10 @@
+// password-gen.js
+const bcrypt = require('bcrypt');
+
+async function generateHashedPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  console.log('Hashed Password:', hashedPassword);
+}
+
+generateHashedPassword("helloworld003@");
